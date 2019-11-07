47aefcb11dc02cc521d4b2a2ca3cfae4308715a2
kamizonomei <m.kamizono@c4c.co.jp>
2019-11-08 06:57:28 +0900

    /*IF org.MAINDISH_FLG == 0*/ 〜  /*END*/で500エラーになるので一旦削除

--- src/main/resources/sql/HC_M_FoodDao_selectRecomFoodByFoodId.sql
+++ src/main/resources/sql/HC_M_FoodDao_selectRecomFoodByFoodId.sql
@@ -1,23 +1,17 @@
 /* HC_M_FoodDao_selectRecomFoodByFoodId.sql */
 select
-  *
+  hc_m_food.*
 from
- hc_m_food,
- (
+  hc_m_food,
+  (
     select
       *
     from
       hc_m_food
     where
       FOOD_ID = /*foodId*/1
- ) org
+  ) org
  where
-  /*IF org.FOOD_ID == 0*/
-  hc_m_food.MAINDISH_FLG = 1 and
-  /*END*/
-  /*IF org.FOOD_ID == 1*/
-  hc_m_food.MAINDISH_FLG = 0 and
-  /*END*/
   hc_m_food.CARBO + org.CARBO < 10 and
   hc_m_food.FAT + org.FAT < 10 and
   hc_m_food.VA + org.VA > 5;
