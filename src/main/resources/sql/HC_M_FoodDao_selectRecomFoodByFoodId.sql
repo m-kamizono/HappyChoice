d1e4c9f4b36f9d601468e72c8b9c77022d648887
kamizonomei <m.kamizono@c4c.co.jp>
2019-11-09 01:07:45 +0900

    SQLを修正 ・インデントの修正 ・メイン（フラグ1）を選んだ場合、おすすめ副菜（フラグ0）となるのでABS（絶対値を算出する関数）を使って1から0, 0から1を指定するようにwhere句を修正 ・ORDER BYを追加

--- src/main/resources/sql/HC_M_FoodDao_selectRecomFoodByFoodId.sql
+++ src/main/resources/sql/HC_M_FoodDao_selectRecomFoodByFoodId.sql
@@ -11,7 +11,10 @@ from
     where
       FOOD_ID = /*foodId*/1
   ) org
- where
+where
+  hc_m_food.MAINDISH_FLG = ABS(org.MAINDISH_FLG - 1) and
   hc_m_food.CARBO + org.CARBO < 10 and
   hc_m_food.FAT + org.FAT < 10 and
-  hc_m_food.VA + org.VA > 5;
+  hc_m_food.VA + org.VA > 5
+order by
+  hc_m_food.FOOD_NAME_KANA asc
