286a9d6121948b5ce263ee40ea7b15f14f2e4142
naho-takahashi <n.takahashi@c4c.co.jp>
2019-11-30 14:51:24 +0900

    めいちゃんの書いたソースに修正

--- src/main/java/jp/co/c4c/service/entity/FoodEntity.java
+++ src/main/java/jp/co/c4c/service/entity/FoodEntity.java
@@ -1,15 +1,10 @@
 package jp.co.c4c.service.entity;
 
-import java.util.ArrayList;
 import java.util.Base64;
 import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.util.HashMap;
 import java.util.Map;
-import java.util.Map.Entry;
-import java.util.TreeMap;
-
-import org.apache.commons.lang3.StringUtils;
+import java.util.stream.Collectors;
 
 import jp.co.c4c.db.dto.HC_M_FoodDto;
 
@@ -118,7 +113,7 @@ public class FoodEntity {
 
         // 不足栄養素メッセージを設定する処理
         if (lessNutFlg) {
-            Map<String, Integer> nutMap = new TreeMap<String, Integer>();
+            Map<String, Integer> nutMap = new HashMap<String, Integer>();
             nutMap.put("炭水化物", foodDto.getCarbo());
             nutMap.put("タンパク質", foodDto.getProt());
             nutMap.put("脂質", foodDto.getFat());
@@ -131,31 +126,15 @@ public class FoodEntity {
             nutMap.put("食物繊維", foodDto.getFib());
             nutMap.put("食塩相当量", foodDto.getSalt());
 
-            // 昇順でのソート
-            List<Entry<String, Integer>> list_entries = new ArrayList<Entry<String, Integer>>(nutMap.entrySet());
-            Collections.sort(list_entries, new Comparator<Entry<String, Integer>>() {
-                public int compare(Entry<String, Integer> obj1, Entry<String, Integer> obj2) {
-                    return obj1.getValue().compareTo(obj2.getValue());
-                }
-            });
+            int minNutNum = Collections.min(nutMap.values());
+            String lessNutNames = nutMap
+                    .entrySet()
+                    .stream()
+                    .filter( entry -> { return minNutNum == entry.getValue(); })
+                    .map( minNut -> minNut.getKey() )
+                    .collect(Collectors.joining("と"));
 
-            // 文字列作成
-            String nutStr = "";
-            int preNut = 0;
-            for(Entry<String, Integer> entry : list_entries) {
-                System.out.println(entry.getKey() + " : " + entry.getValue());
-                if (StringUtils.isEmpty(nutStr)) {
-                    nutStr += entry.getKey();
-                    preNut = entry.getValue();
-                    continue;
-                }
-                // 前の栄養素の値と今回の栄養素の値が違う場合、ループを抜ける
-                if (preNut != entry.getValue()) break;
-                // 前の栄養素の値と今回の栄養素の値が同じ場合、栄養素名を入れる
-                nutStr = nutStr + "と" + entry.getKey();
-                preNut = entry.getValue();
-            }
-            this.lessNutMsg = "不足している" + nutStr + "を補う食品を食べよう！";
+            this.lessNutMsg = "不足している" + lessNutNames + "を補う食品を食べよう！";
         }
     }
 
